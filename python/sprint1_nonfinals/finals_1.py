8bf90615ef95ffe033ed26e6f876357b516653aa
Andrey Fedotov <fedotov_aa@outlook.com>
2022-05-11 08:41:39 +0300

    finals_done_to_review_2

--- python/sprint1_nonfinals/finals_1.py
+++ python/sprint1_nonfinals/finals_1.py
@@ -1,34 +1,42 @@
-# ID 68147413
-def main():
+# ID 68317697
+def nearest_zero(n: int, numbers: list) -> list:
     """Финальное задание 15-го спринта. А) Ближайший ноль."""
 
-    n = int(input())
-    numbers = list(map(int, list(input().split())))
-    null_indexes = [i for i in range(0, n) if numbers[i] == 0] # формируем список с индексами всех нулей
+    # формируем список с индексами всех нулей
+    null_indexes = [i for i in range(0, n) if numbers[i] == 0]
     null_amount = len(null_indexes)
     null_distances = []
     current_null = 0
-    is_first_null = True # флаг для первого нуля, если True - мы не переходим к следующему индексу нуля
+    # флаг для первого нуля, если True - мы не переходим к следующему нулю
+    is_first_null = True
     for i, number in enumerate(numbers):
-        if number == 0: # если встречается ноль, добавляем 0 в результирующий список, обновляем итератор current_null
+        # если встречается ноль, добавляем 0 в результирующий список,
+        # обновляем итератор current_null
+        if number == 0:
             null_distances.append(0)
             if current_null == 0 and is_first_null:
                 is_first_null = False
             else:
                 current_null += 1
         else:
+            # пока ноль не последний - мы выбираем между двух соседних нулей,
+            # который ближе
             if current_null != null_amount - 1:
                 min_distance = min(
                     abs(null_indexes[current_null] - i),
                     abs(null_indexes[current_null + 1] - i)
-                ) # пока ноль не последний - мы выбираем между двух соседних нулей, который ближе
+                )
                 null_distances.append(min_distance)
             else:
                 null_distances.append(abs(null_indexes[current_null] - i))
 
-    print(' '.join(map(str, null_distances)))
+    return null_distances
 
 
 if __name__ == '__main__':
 
-    main()
+    amount = int(input())
+    houses = list(map(int, list(input().split())))
+    distances_to_vacant_lot = nearest_zero(amount, houses)
+    to_print = ' '.join(map(str, distances_to_vacant_lot))
+    print(to_print)
--- python/sprint1_nonfinals/finals_2.py
+++ python/sprint1_nonfinals/finals_2.py
@@ -1,24 +1,29 @@
-# ID 68148637
-def main():
+# ID 68317816
+def get_score(k: int, digits: list) -> int:
     """Финальное задание 15-го спринта. B) Ловкость рук."""
 
-    k = int(input()) * 2
-    digits = []
-    for i in range(4):
-        digits += [int(d) for d in input() if d.isdigit()]
     frequency_dict = {}
     score = 0
+    # считаем количество вхождений каждой цифры
     for digit in digits:
         if digit in frequency_dict:
             frequency_dict[digit] += 1
         else:
             frequency_dict[digit] = 1
+    # считаем итоговую оценку, - это количество цифр, число вхождений которых
+    # меньше или равно k
     for frequency in frequency_dict.values():
         if frequency <= k:
             score += 1
-    print(score)
+
+    return score
 
 
 if __name__ == '__main__':
 
-    main()
+    required_quantity = int(input()) * 2
+    keys = []
+    for i in range(4):
+        keys += [int(d) for d in input() if d.isdigit()]
+    players_score = get_score(required_quantity, keys)
+    print(players_score)
